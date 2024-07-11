baf3b139d3a464cc2e150fc4f14335c1d410e4a5
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-07-11 13:44:42 -0400

    Add types and new enpoint for post notes to database

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,8 +1,45 @@
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma/db'
 
+interface bodyRequestPost {
+  todoText: string
+  todoDescription?: string
+  isCheck?: boolean
+}
+
 export async function GET () {
   const todos = await prisma.todos.findMany()
 
   return NextResponse.json(todos)
+}
+
+export async function POST (request: Request) {
+  let body: bodyRequestPost
+
+  try {
+    body = await request.json()
+  } catch {
+    return NextResponse.json({ message: 'Not are recieve a body' }, { status: 404 })
+  }
+
+  if (!body.todoText) {
+    return NextResponse.json(
+      { message: 'Not are recieve todoText variable' },
+      { status: 404 }
+    )
+  }
+
+  const newTodo = await prisma.todos.create({
+    data: body
+  })
+
+  return NextResponse.json(
+    {
+      message: 'A Todo created Correctly',
+      data: newTodo
+    },
+    {
+      status: 201
+    }
+  )
 }
