d55f20b8d44ba1c88e310b4beb19f507768f5efb
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-07-11 10:13:29 -0400

    Add Fetch to database and component for list todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-export default function Home () {
+import { ListAllTodos } from '@/components/list-all-todos'
+import { todosArray } from './types/todo.types'
+
+async function fetchTodos () {
+  const res = await fetch('http://localhost:3000/api/todos', { cache: 'no-store' })
+
+  return await res.json()
+}
+
+export default async function HomePage () {
+  const allTodos: todosArray = await fetchTodos()
+
   return (
-    <main />
+    <main>
+      <div className='flex'>
+        <ListAllTodos allTodos={allTodos} />
+      </div>
+    </main>
   )
 }
